1249f93f3e42e76ae034240331976747efac1f62
blabs-milan-varshney-127 <milan.varshney@benthonlabs.com>
2025-12-06 14:03:04 +0530

    changes

---
+++ currency-project/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div 
+      style={{
+            backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
+        }}
+    >
+      <div className="w-full">
+        <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+          <form action="">
+            <div className="w-full mb-1">
+                <InputBox
+                    label="From"
+                    onAmountChange={(amount) => setAmount(amount)}
+                    // amount={amount}
+                    // currencyOptions={options}
+                    // onCurrencyChange={(currency) => setAmount(amount)}
+                    // selectCurrency={from}
+                />
+              </div>
+               <div className="relative w-full h-0.5">
+                  <button
+                      type="button"
+                      className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                      // onClick={swap}
+                  >
+                      swap
+                  </button>
+                </div>
+                <div className="w-full mt-1 mb-4">
+                    <InputBox
+                        label="To"
+                        amount={convertedAmount}
+                        currencyOptions={options}
+                        onCurrencyChange={(currency) => setTo(currency)}
+                        selectCurrency={from}
+                        
+                    />
+                </div>
+                <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                    Convert {from.toUpperCase()} to {to.toUpperCase()}
+                </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
