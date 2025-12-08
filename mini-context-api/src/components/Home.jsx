d5a3acc58d45a757fd1527fa2779fc53e94b0298
blabs-milan-varshney-127 <milan.varshney@benthonlabs.com>
2025-12-08 23:30:45 +0530

    changes

--- mini-context-api/src/components/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Home(){
-    return (
-        <>
-        <h1>Welcome To home Page</h1>
-        </>
-    )
-}
--- mini-context-api/src/components/index.js
+++
@@ -1,7 +0,0 @@
-import Header from './Header'
-import Layout from './Layout'
-import Login from './Login'
-import Profile from './Profile'
-import Home from './Home'
-
-export {Home, Header, Layout, Login, Profile}
